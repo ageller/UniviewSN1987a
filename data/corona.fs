af061a19bd15548095bb7030f2810e27e321f682
Aaron Geller <a-geller@northwestern.edu>
2018-10-04 13:58:46 -0500

    added star

---
+++ data/corona.fs
@@ -0,0 +1,104 @@
+uniform float uv_fade;
+uniform mat4 uv_modelViewInverseMatrix;
+
+uniform float coronaTeff;
+uniform sampler2D bb;
+
+in vec2 texcoord;
+
+out vec4 fragColor;
+
+//from https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+//simple 3D noise
+float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+float snoise(vec3 p){
+	vec3 a = floor(p);
+	vec3 d = p - a;
+	d = d * d * (3.0 - 2.0 * d);
+
+	vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+	vec4 k1 = perm(b.xyxy);
+	vec4 k2 = perm(k1.xyxy + b.zzww);
+
+	vec4 c = k2 + a.zzzz;
+	vec4 k3 = perm(c);
+	vec4 k4 = perm(c + 1.0);
+
+	vec4 o1 = fract(k3 * (1.0 / 41.0));
+	vec4 o2 = fract(k4 * (1.0 / 41.0));
+
+	vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+	vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+
+	return o4.y * d.y + o4.x * (1.0 - d.y);
+}
+
+// from https://www.seedofandromeda.com/blogs/49-procedural-gas-giant-rendering-with-gpu-noise
+//fractal noise
+float noise(vec3 position, int octaves, float frequency, float persistence, int rigid) {
+	float total = 0.0; // Total value so far
+	float maxAmplitude = 0.0; // Accumulates highest theoretical amplitude
+	float amplitude = 1.0;
+	const int largeN = 50;
+	for (int i = 0; i < largeN; i++) {
+		if (i > octaves){
+				break;
+		}
+		// Get the noise sample
+		if (rigid == 0){
+		   total += snoise(position * frequency) * amplitude;
+		} else {
+		// rigid noise
+			total += ((1.0 - abs(snoise(position * frequency))) * 2.0 - 1.0) * amplitude;
+		}
+		// Make the wavelength twice as small
+		frequency *= 2.0;
+		// Add to our maximum possible amplitude
+		maxAmplitude += amplitude;
+		// Reduce amplitude according to persistence for the next octave
+		amplitude *= persistence;
+	}
+
+	// Scale the result by the maximum amplitude
+	return total / maxAmplitude;
+}
+
+void main()
+{
+	fragColor = vec4(1.);
+	
+	vec2 fromCenter = texcoord * 2 - vec2(1);
+	float dist = dot(fromCenter,fromCenter);
+	float angle = atan((texcoord.x - 0.5)*2., (texcoord.y - 0.5)*2.);
+
+	float u = 1.;
+	float Rstar2 = 1.0;
+	float brightness = (1. - u*(1. - sqrt((Rstar2 - dist)/Rstar2)));
+	fragColor.a *= uv_fade;
+
+	vec3 cameraPosition = (uv_modelViewInverseMatrix * vec4(0, 0, 0, 1)).xyz;
+
+	vec3 cNorm = normalize(cameraPosition);
+	vec3 pNorm = vec3(angle, 0., 1.) + cNorm;
+
+// Offset distance with noise
+	float cn = noise(pNorm, 4, 3., 0.7, 0);
+	float distN = dist/cn;
+
+	float alpha = 1.;
+	float dlim = 0.0005;
+	if (dist >= dlim){
+		alpha = (1.0 - pow(distN+dlim, 0.1))*2.;
+	}
+	
+	fragColor.rgb = texture(bb, vec2(clamp(((coronaTeff - 1000.)/19000.),0.,1.),0.5)).rgb * brightness;
+	fragColor.rgb = vec3(mix(vec3(1.0), fragColor.rgb, clamp(pow(dist, 0.2) ,0. , 1.)));
+	fragColor.a = (1.0 - pow(dist+dlim, 0.1))*2.;
+	
+	fragColor.a *= alpha;
+
+
+
+}
---
+++ data/corona.gs
@@ -0,0 +1,62 @@
+layout(triangles) in; //seems necessary to have this even if I don't need any data?
+layout(triangle_strip, max_vertices = 4) out;
+
+uniform mat4 uv_modelViewProjectionMatrix;
+uniform mat4 uv_modelViewInverseMatrix;
+
+
+uniform sampler2D stateTexture;
+
+//uniform float eventTime;
+uniform float coronaRad;
+uniform float coronaEndTime;
+
+out vec2 texcoord;
+
+
+// axis should be normalized
+mat3 rotationMatrix(vec3 axis, float angle)
+{
+	float s = sin(angle);
+	float c = cos(angle);
+	float oc = 1.0 - c;
+	
+	return mat3(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,
+				oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,
+				oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c);
+}
+
+void drawSprite(vec4 position, float radius, float rotation)
+{
+	vec3 objectSpaceUp = vec3(0, 0, 1);
+	vec3 objectSpaceCamera = (uv_modelViewInverseMatrix * vec4(0, 0, 0, 1)).xyz;
+	vec3 cameraDirection = normalize(objectSpaceCamera - position.xyz);
+	vec3 orthogonalUp = normalize(objectSpaceUp - cameraDirection * dot(cameraDirection, objectSpaceUp));
+	vec3 rotatedUp = rotationMatrix(cameraDirection, rotation) * orthogonalUp;
+	vec3 side = cross(rotatedUp, cameraDirection);
+	texcoord = vec2(0, 1);
+	gl_Position = uv_modelViewProjectionMatrix * vec4(position.xyz + radius * (-side + rotatedUp), 1);
+	EmitVertex();
+	texcoord = vec2(0, 0);
+	gl_Position = uv_modelViewProjectionMatrix * vec4(position.xyz + radius * (-side - rotatedUp), 1);
+	EmitVertex();
+	texcoord = vec2(1, 1);
+	gl_Position = uv_modelViewProjectionMatrix * vec4(position.xyz + radius * (side + rotatedUp), 1);
+	EmitVertex();
+	texcoord = vec2(1, 0);
+	gl_Position = uv_modelViewProjectionMatrix * vec4(position.xyz + radius * (side - rotatedUp), 1);
+	EmitVertex();
+	EndPrimitive();
+}
+
+void main()
+{
+
+	float eventTime = texture(stateTexture, vec2(0.5)).r;
+	if (eventTime <= coronaEndTime){
+		float rad = coronaRad*clamp(coronaEndTime - eventTime, 0, 1);
+		drawSprite(vec4(gl_in[0].gl_Position.xyz, 1.), rad, 0);
+	}
+
+
+}
